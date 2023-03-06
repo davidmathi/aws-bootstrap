2888ecc2c59174d527ea28eb844ea6b2ff29b377
davidmathi <davidlivingstone.mathi@gmail.com>
2023-03-06 14:01:21 -0600

    Add codebuild/deploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -3,8 +3,27 @@
 STACK_NAME=awsbootstrap 
 REGION=us-east-1
 CLI_PROFILE=test-dmathi-aws-profile
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile test-dmathi-aws-profile \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
 
-EC2_INSTANCE_TYPE=t2.micro 
+EC2_INSTANCE_TYPE=t2.micro
+
+
+ # Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
+
+
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
@@ -16,3 +35,10 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+
+      # If the deploy succeeded, show the DNS name of the created instance
+if [ $? -eq 0 ]; then
+  aws cloudformation list-exports \
+    --profile test-dmathi-aws-profile \
+    --query "Exports[?Name=='InstanceEndpoint'].Value" 
+fi
